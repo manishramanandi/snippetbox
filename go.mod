0f8bfe46641c6c21ed14a7dd6af98d61e2e63449
manishramanandi <bairagimanish222@gmail.com>
2024-09-24 18:45:34 +0530

    Satup a server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module manishramanandi.net/snippetbox
+
+go 1.23.1
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	// Define a home handler function writes a byte slice containing
+	// "Hello from snippetbox" as the response body
+	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func main() {
+	// Use the http.NewServeMux() function to initialize a new servemux,
+	// then register the home function as the handler for the "/"URL pattern.
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	// Use the http.ListenAndServe() function to start a new web server. We pass in
+	// two parameters: the TCP network address to listen on :4000 you can change it if you want to
+	// we use the log.Fatal() function to log the error message and exit simple
+	// Note that any error returned by http.ListenAndServer() is always non-nil.
+
+	log.Println("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+
+}
