0af8c09bb27a651c9e006ce8b38e5674e20f71be
manishramanandi <bairagimanish222@gmail.com>
2025-03-14 00:36:28 +0530

    config the base file component for shared content

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -15,12 +15,25 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// initialize a slice containing the paths to the two files.
+	// it is important to note that the file containing our base template must be the first
+	//file in the slice
+
+	files := []string{
+
+		"./ui/html/base.html",
+		"/ui/html/pages/home.html",
+	}
+
 	// Use the template.ParseFiles() function read the template
 	// file into a template set If there is an error, we log the details
 	// error message and use the http.Error() function to see a generic 500
 	// internal server error response to the user.
 
-	ts, err := template.ParseFiles("./ui/html/pages/home.html")
+	// use the template.ParseFiles() function to read the files and store the
+	// templates in a template set. notice that we can pass the slice of file
+
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
